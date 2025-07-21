74c3da69cf9c98e4c492d97d9b502dcaa6972aad
jeromee <bulosanjerome50@gmail.com>
2025-07-21 08:38:43 +0800

    Completing the strapi context api projects

--- Strapi-ContextAPI/src/Navbar.jsx
+++ Strapi-ContextAPI/src/Navbar.jsx
@@ -1,15 +1,21 @@
 import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./Context";
+import Navlinks from "./Navlinks";
 const Navbar = () => {
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext();
+  const handleSubemenu = (e) => {
+    if (!e.target.classList.contains("nav-link")) {
+      setPageId(null);
+    }
+  };
   return (
-    <nav>
+    <nav onMouseOver={handleSubemenu}>
       <div className="nav-center">
         <h3 className="logo">Strapi</h3>
         <button className="toggle-btn" onClick={openSidebar}>
           <FaBars />
         </button>
-        {/* {navlinks later} */}
+        <Navlinks />
       </div>
     </nav>
   );
