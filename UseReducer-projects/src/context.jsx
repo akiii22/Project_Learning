3693dcf11c176dcab84a3ee0b5a0b65219e16722
jeromee <bulosanjerome50@gmail.com>
2025-07-28 18:42:57 +0800

    Adding and Setting up the Context, Reducer and the Action for thr project cart

---
+++ UseReducer-projects/src/context.jsx
@@ -0,0 +1,27 @@
+import { useContext, useReducer, useEffect, createContext } from "react";
+import reducer from "./reducer";
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "../action";
+const AppContext = createContext();
+
+const initialState = {
+  loading: false,
+  cart: [],
+};
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
