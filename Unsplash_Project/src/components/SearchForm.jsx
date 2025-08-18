eea885a14c2d6d51c8cc36a2296021742531e39b
jeromee <bulosanjerome50@gmail.com>
2025-08-18 18:08:20 +0800

    Adding the Search form without using the controlled elements

--- Unsplash_Project/src/components/SearchForm.jsx
+++ Unsplash_Project/src/components/SearchForm.jsx
@@ -1,4 +1,23 @@
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchResult = e.target.elements.search.value;
+    if (!searchResult) return;
+    console.log(searchResult);
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form onSubmit={handleSubmit} className="search-form">
+        <input
+          type="text"
+          name="search"
+          placeholder="cat"
+          className="form-input search-input"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </section>
+  );
 };
 export default SearchForm;
