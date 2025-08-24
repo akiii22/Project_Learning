5a7d7d9d721ffdca44c030652b50ea8d119e6c59
jeromee <bulosanjerome50@gmail.com>
2025-08-24 16:06:23 +0800

    Finishing the Contentful Management prjoect

--- Contentful_Project/src/fetchProjects.jsx
+++ Contentful_Project/src/fetchProjects.jsx
@@ -1,13 +1,37 @@
 import { createClient } from "contentful";
-
+import { useState, useEffect } from "react";
 const client = createClient({
   space: "6so8qkv7xcup",
   environment: "master",
-  accessToken: "BHlJbltqhL49RerKJRvwH-UnNrTJ1nd__dRx1bqKNJU",
+  accessToken: import.meta.env.VITE_API_KEY,
 });
 
-client
-  .getEntries({
-    content_type: "projects",
-  })
-  .then((response) => console.log(response));
+export const useFetchProject = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [projects, setIsProjects] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({
+        content_type: "projects",
+      });
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return { title, url, img, id };
+      });
+      setIsProjects(projects);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { isLoading, projects };
+};
