ffa02f08383dbd897a2bf9349b9836e85b0eb32d
jeromee <bulosanjerome50@gmail.com>
2025-09-02 17:55:15 +0800

    Making a Error message for 404 page not found also making a SinglePage error, setting up the Cocktail list or displaying it on the landing page and also setting up the Single Cocktal page

--- Mixmaster/src/App.jsx
+++ Mixmaster/src/App.jsx
@@ -6,18 +6,25 @@ import {
   HomeLayout,
   Landing,
   Newsletter,
+  SinglePageError,
 } from "./pages";
+
+import { loader as landingLoader } from "./pages/Landing";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
         element: <Landing />,
+        errorElement: <SinglePageError />,
+        loader: landingLoader,
       },
       {
-        path: "/cocktail",
+        path: "/cocktail/:id",
         element: <CockTail />,
       },
       {
--- Mixmaster/src/pages/HomeLayout.jsx
+++ Mixmaster/src/pages/HomeLayout.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isPageLoading = navigation.state === "loading";
   return (
     <>
       <Navbar />
-      <Outlet />
+      <section className="page">
+        {isPageLoading ? <div className="loading"></div> : <Outlet />}
+        <Outlet />
+      </section>
     </>
   );
 };
